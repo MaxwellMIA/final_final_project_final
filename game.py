43da823533488b60d23347f0afa0b092eca27e58
Maxwell <cte-berry.gavigan@sllboces.org>
2026-05-06 14:48:31 +0000

    4

--- game.py
+++ game.py
@@ -7,12 +7,6 @@ def slowprint(text, delay=0.07):
         time.sleep(delay)
     print()
 
-# Changing variables
-
-player_stats = {"Health": 100, "Stress": 0, "Money": 50}
-day = 1
-current_area = "bedroom"
-
 # starting zone
 
 slowprint("~~GAME NAME~~")
@@ -22,70 +16,55 @@ if name == "Scarlet":
     slowprint("Sorry, that names not available")
 
 class Area:
-    def __init__(self):
-        self.name = 0
-        self.description = 0
-        self.exits = 0
-        self.look = 0
-        self.interact = 0
+    def __init__(self, name, description, exits, look, interact):
+        self.name = name
+        self.description = description
+        self.exits = exits
+        self.look = look
+        self.interact = interact
+ 
+def print_area(self):
+    slowprint(f"You are in the {self.name}.")
+    slowprint(self.description)
+    #!!!!!!!!!!reask from here
+    slowprint("What would you like to do?")
+    command = input("~Travel, Look, Interact, stats~ ").lower()
 
-    def print_area(self):
-        slowprint(f"You are in {self.name}.")
-        slowprint(self.description)
-        #!!!!!!!!!!reask from here
-        slowprint("What would you like to do?")
-        command = input("~Travel, Look, Interact, stats~ ").lower()
-        
-        if command == "travel":
-            slowprint("Where do you want to go?")
-            command = input(f"~{self.exits}, back~ ").lower()
-            if command == self.exits:
-                slowprint(f"You walk to {self.exits}")
-                current_area = {self.exits}
-                #!!!!!! add smt to print out next areas stuff
-            if command == "back":
-                slowprint("You've changed your mind.")
-                #!!!!!!!! make it avaible to redo the "what would you like to do" to run though these 3 if statments again
+    if command == "travel":
+        slowprint("Where do you want to go?")
+        command = input(f"~{self.exits}, back~ ").lower()
+        if command == self.exits:
+            slowprint(f"You walk to {self.exits}")
+            current_area = {self.exits}
+        if command == "back":
+            print("hi")
 
-        if command == "look":
-            slowprint(self.look)
-            # make it avaible to redo Nhe "what would you like to do" to run though these 3 if statments again
+    if command == "look":
+        slowprint(self.look)
+        # make it avaible to redo Nhe "what would you like to do" to run though these 3 if statments again
 
-        if command == "interact":
-            slowprint(self.interact)
+    if command == "interact":
+        slowprint(self.interact)
 
-        if command == "stats":
-            slowprint(f"{name}'s stats. Health:{player_stats['Health']} Stress:{player_stats['Stress']} Money:{player_stats['Money']}")
+    if command == "stats":
+        slowprint(f"{name}'s stats. Health:{player_stats['Health']} Stress:{player_stats['Stress']} Money:{player_stats['Money']}")
 
-bedroom = Area()
-bedroom.name = "Bedroom"
-bedroom.description = "It's your bedroom. It seems messier then it was yesterday."
-bedroom.exits = "Hallway"
-bedroom.look = "It's your childhood bedroom, the walls have faded in color over the years. Trash litters your desk and the floor, while your bed is covered in dirty clothes you haven't feel like moving."
-bedroom.interact = "Bed, Computer, Bookshelf"
+# dog1 = Dog("Buddy", 3)
+# dog2 = Dog("Lucy", 5)
 
-hallway = Area()
-hallway.name = "Hallway"
-hallway.description = "A short hallway."
-hallway.exits = "Bedroom, Bathroom, Stairs"
-hallway.look = "There is spots of paint missing from the wall where paintings used to hang. Most of the photos got taken down after the divorce"
-hallway.interact = "Plant"
+bedroom = Area("Bedroom", "It's your bedroom. It seems messier then it was yesterday.", "Hallway", "It's your childhood bedroom, the walls have faded in color over the years. Trash litters your desk and the floor, while your bed is covered in dirty clothes you haven't feel like moving.", "Bed, Computer, Bookshelf")
 
-bathroom = Area()
-bathroom.name = "Bathroom"
-bathroom.description = "A plain bathroom. It's cleaner then your room."
-bathroom.exits = "Hallway"
-bathroom.look = "The mirror on the wall taunts you, you feel a wave of dread come over you."
-bathroom.interact = "Mirror"
+hallway = Area("Hallway", "A short hallway.", "Bedroom, Bathroom, Stairs", "There is spots of paint missing from the wall where paintings used to hang. Most of the photos got taken down after the divorce", "Plant")
 
-stairs = Area()
-stairs.name = "Stairs"
-stairs.description = "You're always afriad of slipping"
-stairs.exits = "Hallway, Living room"
-stairs.look = "You look at the painting on the wall, but you don't know what it is."
-stairs.interact = "There is nothing to interact with."
+bathroom = Area("Bathroom", "A plain bathroom. It's cleaner then your room.", "Hallway", "The mirror on the wall taunts you, you feel a wave of dread come over you.", "Mirror")
 
-livingroom = Area()
-livingroom.name = "Living room"
-livingroom.description = "The living room seems darker then normal."
-livingroom.exits = "Stairs, Kitchen, Yard,"
+stairs = Area("Stairs", "You're always afriad of slipping", "Hallway, Living room", "You look at the painting on the wall, but you don't know what it is.", "Painting")
+
+livingroom = Area("Living room", "The living room seems darker then normal.", "Stairs, Kitchen, Yard,", "Livingroomlook", "Livingroominteract")
+
+
+
+player_stats = {"Health": 100, "Stress": 0, "Money": 50}
+day = 1
+current_area = bedroom
+print_area(current_area)
